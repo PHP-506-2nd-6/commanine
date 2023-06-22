8a37956cfb5945f03eaeb120e7840233d0cb6e40
yovjvng <yoyo0249@naver.com>
2023-06-22 18:16:54 +0900

    0622(메인css)백유정

--- commanine/resources/views/main.blade.php
+++ commanine/resources/views/main.blade.php
@@ -66,6 +66,114 @@
         </li>
     </ul>
 </div>
+
+
+
+
+
+
+
+
+
+{{-- test --}}
+{{-- <section id="menu2" class="content02">
+                <div class="split myCarousel">
+                    <div class="inner-wrapper">
+                        <div class="slides-wrapper">
+                            <div class="slides content">
+                                <div class="slide">
+                                    <div class="bodytext">
+                                        <h2 class="title">vfx</h2>
+                                        <p class="subtitle">LOCUS는 최고의 VFX 기술을 보유하고 모든 미디어 플랫폼을 위한 <br> 컨텐츠 제작 및 크리에이티브 솔루션을 제공합니다.</p>
+                                        <div class="tag">
+                                            <span class="tag_01"># 3D Animation</span>
+                                            <!--                                            <span class="tag_01"># Visual Experience</span>-->
+                                            <span class="tag_01"># Game Cinematic</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="slide">
+                                    <div class="bodytext">
+                                        <h2 class="title">animation</h2>
+                                        <p class="subtitle">LOCUS는 세계 최고 수준의 3D 애니메이션 제작 역량을 보유하고 <br> 다양한 TV, 극장용 애니메이션 제작을 진행하고 있습니다.</p>
+                                        <div class="tag">
+                                            <span class="tag_02"># RED SHOES AND THE 7 DWARFS</span>
+                                            <span class="tag_02"># RUNNINGMAN</span>
+                                            <!--                                            <span class="tag_02"># 유미의 세포</span>-->
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="slide">
+                                    <div class="bodytext">
+                                        <h2 class="title">character</h2>
+                                        <p class="subtitle">LOCUS는 자체 창작 캐릭터 라이선싱 사업을 통해 <br>다양한 상품과 콘텐츠를 기획/출시하고 있습니다.</p>
+                                        <div class="tag">
+                                            <span class="tag_03"># BOOTO</span>
+                                            <!--                                            <span class="tag_03"># DESIGN</span>-->
+                                            <span class="tag_03"># Licensing Business</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            <div class="slides photo">
+                                <div class="slide">
+                                    <div class="image">
+                                        <img src="https://img.freepik.com/free-photo/cloud-and-blue-sky_1150-35749.jpg" alt="">
+                                    </div>
+                                </div>
+                                <div class="slide">
+                                    <div class="image">
+                                        <img src="https://img.freepik.com/free-photo/cloud-and-blue-sky_1150-35749.jpg" alt="">
+                                    </div>
+                                </div>
+                                <div class="slide">
+                                    <div class="image">
+                                        <img src="https://img.freepik.com/free-photo/cloud-and-blue-sky_1150-35749.jpg" alt="">
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="slides title">
+                                <div class="slide red">
+                                    <div class="bodytext ">
+                                        <a class="link" href="#"><span>View more</span></a>
+                                    </div>
+                                </div>
+                                <div class="slide blue">
+                                    <div class="bodytext ">
+                                        <a class="link" href="#"><span>View more</span></a>
+                                    </div>
+                                </div>
+                                <div class="slide green">
+                                    <div class="bodytext ">
+                                        <a class="link" href="#"><span>View more</span></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div id="arrows" class="navigation-wrapper">
+                            <a href="#" class="navigation-arrow prev"><i class="far fa-caret-square-left" aria-hidden="true"></i></a>
+                            <!--                            <span>.</span>-->
+                            <a href="#" class="navigation-arrow next"><i class="far fa-caret-square-right" aria-hidden="true"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </section> --}}
+
+
+
+
+
+
+
+
+
+
+
+
 {{-- 인기숙소 --}}
 <div id="carouselExample" class="carousel slide" data-bs-touch="false" data-bs-interval="false">
             <div class="carousel-inner">
@@ -164,10 +272,21 @@
 
 </main>
 
+<script src="{{asset('js/main.js')}}"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
 <script
     src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa"
-    crossorigin="anonymous"
-    ></script>
+    crossorigin="anonymous"></script>
+<script src="https://kit.fontawesome.com/4c8256fa0a.js" crossorigin="anonymous"></script>
+<script>
+            $(".myCarousel").split({
+                theme: "right-to-left ,right-to-left,right-to-left,right-to-left ,fade-in-out",
+                delay: "50",
+                height: "600",
+                infinite: "true",
+            });
+        </script>
     
 @endsection
