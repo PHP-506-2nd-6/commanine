324101894f84996c9672133b8f9adeaa778606da
sangheum1 <js040141@naver.com>
2023-06-27 13:08:26 +0900

    0627(결제페이지css) 윤상흠

--- commanine/resources/views/payCompInfo.blade.php
+++ commanine/resources/views/payCompInfo.blade.php
@@ -1,43 +1,66 @@
-<?php
+@extends('layout.layout')
 {{-- 요일 변환 해주는 변수 지정 --}}
+<?php
 $yoil = array("일","월","화","수","목","금","토");
 ?>
-
-{{substr($data[0]->chk_in_date,0,10).'('.$yoil[date('w', strtotime(substr($data[0]->chk_in_date,0,10)))].')'.$data[0]->chk_in}}
-
-<div>
-    <h1>예약이 완료되었습니다.</h1>
-    <p>예약일시 : {{substr($data[0]->updated_at,0,10).' ('.$yoil[date('w', strtotime(substr($data[0]->chk_in_date,0,10)))].') '.substr($data[0]->updated_at,-8)}}</p>
-    <p>숙소 예약 번호 : {{$data[0]->id}}</p>
-</div>
-<div>
-    <img src="{{asset($data[0]->room_img1)}}" alt="{{asset($data[0]->hanok_name)}}">
-    <p>{{$data[0]->hanok_name}}</p>
-    <p>{{$data[0]->room_name.' / '.$data[0]->reserve_adult.'명'}}</p>
-    <p>숙소 문의 : {{$data[0]->hanok_num}}</p>
-</div>
-<div>
-    <div>
-        <p>체크인</p>
-        <p>
-            {{substr($data[0]->chk_in_date,0,10).' ('.$yoil[date('w', strtotime(substr($data[0]->chk_in_date,0,10)))].') '.$data[0]->chk_in}}
-        </p>
+<head>
+    <link rel="stylesheet" href="{{asset('css/paycompinfo.css')}}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous"/>
+</head>
+@section('contents')
+{{-- {{substr($data[0]->chk_in_date,0,10).'('.$yoil[date('w', strtotime(substr($data[0]->chk_in_date,0,10)))].')'.$data[0]->chk_in}} --}}
+<div class="main_container">
+    <div class="size_container">
+        <p class="font_size1">예약이 완료되었습니다.</p>
+        <p>예약일시 : {{substr($data[0]->updated_at,0,10).' ('.$yoil[date('w', strtotime(substr($data[0]->chk_in_date,0,10)))].') '.substr($data[0]->updated_at,-8)}}</p>
+        <p class="font_size2">숙소 예약 번호 : {{$data[0]->id}}</p>
     </div>
-    <div>
-        <p>체크아웃</p>
-        <p>
-            {{substr($data[0]->chk_out_date,0,10).' ('.$yoil[date('w', strtotime(substr($data[0]->chk_out_date,0,10)))].') '.$data[0]->chk_out}}
-        </p>
+    <div class="flex_container">
+        <div class="items"><img src="{{asset($data[0]->room_img1)}}" alt="{{asset($data[0]->hanok_name)}}" class="flex_img"></div>
+        <div class="items flex_items2">
+            <div class="flex_size1">{{$data[0]->hanok_name}}</div>
+            <div class="flex_size2">{{$data[0]->hanok_addr}}</div>
+            <div>{{$data[0]->room_name.' / '.$data[0]->reserve_adult.'명'}}</div>
+            <div class="flex_size3">숙소 문의 : {{$data[0]->hanok_num}}</div>
+        </div>
+    </div>
+    <div class="flex_container2">
+        <div class="items2 flex2_size2">
+            <p class="flex2_size1">체크인</p>
+            <p>
+                {{substr($data[0]->chk_in_date,0,10).' ('.$yoil[date('w', strtotime(substr($data[0]->chk_in_date,0,10)))].') '.$data[0]->chk_in}}
+            </p>
+        </div>
+        <div class="flex2_circle">
+            <div class="circle_item">
+                1박
+            </div>
+        </div>
+        <div class="items2">
+            <p class="flex2_size1">체크아웃</p>
+            <p>
+                {{substr($data[0]->chk_out_date,0,10).' ('.$yoil[date('w', strtotime(substr($data[0]->chk_out_date,0,10)))].') '.$data[0]->chk_out}}
+            </p>
+        </div>
+    </div>
+    <div class="pay_container">
+        <p class="pay_font_size1">결제 정보</p>
+        <div class="grid_container1">
+            <p>상품가격</p>
+            <p class="pay_font_size2">{{$data[0]->pay_price}}</p>
+        </div>
+        <div class="grid_container1">
+            <p>실 결제 금액</p>
+            <p class="pay_font_size2 pay_color">{{$data[0]->pay_price}}</p>
+        </div>
+        <div class="grid_container1">
+            <p>결제 수단</p>
+            <p class="pay_font_size3">{{$data[0]->pay_type}}</p>
+        </div>
+    </div>
+    <div class="btn_group">
+    <button type="button" class="btn1" onclick="location.href='{{route('main')}}'">결제 확인</button>
+    <button type="button" class="btn2" onclick="location.href='{{route('users.information.reserve')}}">예약 내역 보기</button>
     </div>
 </div>
-<div>
-    <p>결제 정보</p>
-    <p>상품가격</p>
-    <p>{{$data[0]->pay_price}}</p>
-    <p>실 결제 금액</p>
-    <p>{{$data[0]->pay_price}}</p>
-    <p>결제 수단</p>
-    <p>{{$data[0]->pay_type}}</p>
-</div>
-<button type="button" onclick="location.href='{{route('main')}}'">메인 페이지 이동</button>
-<button type="button" onclick="location.href='{{route('users.information.reserve')}}">예약 내역 페이지 이동</button>
+@endsection
