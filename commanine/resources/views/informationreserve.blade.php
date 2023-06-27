d6d0f0029bd7f97549e3ee7a45b0d31a202f5667
yovjvng <yoyo0249@naver.com>
2023-06-27 13:08:42 +0900

    0627(css)백유정

--- commanine/resources/views/informationreserve.blade.php
+++ commanine/resources/views/informationreserve.blade.php
@@ -1,50 +1,45 @@
 @extends('layout.layout')
 <head>
     <link rel="stylesheet" href="{{asset('css/informationreserve.css')}}">
+    <link rel="stylesheet" href="{{asset('css/sidebar.css')}}">
+    {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"> --}}
 </head>
 @section('contents')
-    <div class="wrap">
-        <div>
-            <ul>
-                <li><a href="{{route('users.information.reserve')}}">예약 내역</a></li>
-                {{-- <li><a href="{{route('users.information.dibs')}}">찜 목록</a></li> --}}
-                <li><a href="{{route('users.information.info')}}">회원 정보</a></li>
-                {{-- <li><a href="{{route('users.information.review')}}">내 리뷰</a></li> --}}
-            </ul>
-        </div>
-        <div class="reserve_wrap">
-            <h1>예약 내역</h1>
+<div class="wrap wrapGrid">
+        @include('layout.sidebar')
+        <div class="reserve_box">
+            <h2>예약 내역</h2>
+            <div class="reserve_cont">
                 @forelse($reserve as $data)
-            <div class="reserve_con">
-                <div class="con reserve_img">
-                    <img src="{{asset($data->hanok_img1)}}" alt="#">
-                </div>
-                <div class="con">
-                    {{-- <div>아이디</div>
-                    <div><p>{{$data->user_id}}</p></div> --}}
-                    <div>숙소명</div>
-                    <div><p>{{$data->hanok_name}}</p></div>
-                    <div>객실명</div>
-                    <div><p>{{$data->room_name}}</p></div>
-                    <div>가격</div>
-                    <div>{{$data->room_price}}</div>
-                    <div>예약 날짜</div>
-                    <div><p>{{$data->chk_in}} ~ {{$data->chk_out}}</p></div>
-                    <div>인원</div>
-                    <div><p>{{$data->reserve_adult}}</p></div>
-                </div>
-                
-                {{-- @if($val->room_id === 0) --}}
-                    
-                    {{-- <a id="writeButton" href="{{route('users.review')}}">리뷰 작성하기</a> --}}
+                <div class="reserve_content">
+                    <div class="con reserve_img">
+                        <img src="{{asset($data->hanok_img1)}}" alt="#">
+                    </div>
+                    <div class="con">
+                        <div>숙소명</div>
+                        <div><p>{{$data->hanok_name}}</p></div>
+                        <div>객실명</div>
+                        <div><p>{{$data->room_name}}</p></div>
+                        <div>가격</div>
+                        <div>{{$data->room_price}}</div>
+                        <div>예약 날짜</div>
+                        <div><p>{{$data->chk_in}} ~ {{$data->chk_out}}</p></div>
+                        <div>인원</div>
+                        <div><p>{{$data->reserve_adult}}</p></div>
+                    </div>
 
-                {{-- <button id="writeButton" onclick="checkDataAndRedirect()">작성하기</button> --}}
-                {{-- <button onclick="location.href='{{ route('check-data-and-redirect') }}'">작성하기</button> --}}
-                {{-- @endif --}}
+                    {{-- @if($val->room_id === 0) --}}
+
+                         <a id="writeButton" href="{{route('users.review')}}">리뷰 작성하기</a>
+
+                    {{-- <button id="writeButton" onclick="checkDataAndRedirect()">작성하기</button> --}}
+                    {{-- <button onclick="location.href='{{ route('check-data-and-redirect') }}'">작성하기</button> --}}
+                    {{-- @endif --}}
+                </div>
             </div>
-        @empty
-            <span>예약한 숙소가 없습니다.</span> 
-        @endforelse
+            @empty
+                <span>예약한 숙소가 없습니다.</span> 
+            @endforelse
         </div>
     </div>
 
