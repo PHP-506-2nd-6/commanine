b9f76de6899c118b17ee1af9485288194c8cdcb7
KangMinjin <kmj_7809@naver.com>
2023-07-27 12:54:22 +0900

    0727(관리자 유저 페이지 수정)강민진

--- commanine/resources/views/adminUser.blade.php
+++ commanine/resources/views/adminUser.blade.php
@@ -9,11 +9,36 @@
         <div class="container col-9 content-box">
             <h2 class="top-title">유저 관리</h2>
             <form action="{{route('admin.users.search')}}" method="get" class="form" >
-                <input type="text" placeholder="이메일/이름 입력" name="users" class="input">
+                <div>
+                    <label>
+                        <input type="radio" name="user_status" value="" {{$userStatus === "" || $userStatus === null ? "checked" : ""}}>
+                        전체회원
+                    </label>
+                    <label>
+                        <input type="radio" name="user_status" value="0" {{$userStatus === "0" ? "checked" : ""}}>
+                        일반
+                    </label>
+                    <label>
+                        <input type="radio" name="user_status" value="1" {{$userStatus === "1" ? "checked" : ""}}>
+                        차단
+                    </label>
+                    <label>
+                        <input type="radio" name="user_status" value="9" {{$userStatus === "9" ? "checked" : ""}}>
+                        탈퇴
+                    </label>
+                </div>
+                <input type="text" placeholder="이메일/이름 입력" name="users" class="input" value="{{request('users')}}">
                 <button type="submit" class="btn btn-dark">검색</button>
             </form>
-            <div >
-                {{-- <span class="btnInfo">리셋 정지 복구</span> --}}
+            <div>
+                <div class="btnInfo">
+                    <div class="info reset"></div>
+                    <span class="infoText">리셋</span>
+                    <div class="info ban"></div>
+                    <span class="infoText">정지</span>
+                    <div class="info cancel"></div>
+                    <span class="infoText">복구</span>
+                </div>
                 <table class="table">
                     <thead >
                         <tr>
@@ -23,6 +48,7 @@
                             <th scope="col">전화번호</th>
                             <th scope="col">가입일자</th>
                             <th scope="col">탈퇴일자</th>
+                            <th scope="col">상태</th>
                             <th scope="col">비밀번호 리셋</th>
                             <th scope="col">회원정지</th>
                         </tr>
@@ -36,6 +62,15 @@
                             <td class="td_chk">{{$val->user_num}}</td>
                             <td class="td_chk">{{$val->created_at}}</td>
                             <td class="td_chk">{{isset($val->deleted_at) ? $val->deleted_at : ""}}</td>
+                            <td class="td_chk">
+                                @if($val->user_status == '0')
+                                    <span>정상</span>
+                                @elseif($val->user_status == '1')
+                                    <span style="color:red">차단</span>
+                                @else
+                                    <span style="color:#ccc">탈퇴</span>
+                                @endif
+                            </td>
                             <td class="td_chk">
                                 <form class="frm" action="{{route('admin.users.pw.reset',['user_id' => $val->user_id])}}" method="POST"
                                     onsubmit="return confirm('{{$val->user_name}}회원의 비밀번호를 리셋하시겠습니까?');">
